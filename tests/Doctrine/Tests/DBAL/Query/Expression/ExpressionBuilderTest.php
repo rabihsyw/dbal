6d0719ed2cc0b64ca8f2140595afc9cb0d7ffafc
Rabih Abou Zaid <rabihsyw@gmail.com>
2017-05-04 12:36:38 +0200

    update expressionBuilder testIn

--- tests/Doctrine/Tests/DBAL/Query/Expression/ExpressionBuilderTest.php
+++ tests/Doctrine/Tests/DBAL/Query/Expression/ExpressionBuilderTest.php
@@ -199,7 +199,7 @@ class ExpressionBuilderTest extends \Doctrine\Tests\DbalTestCase
 
     public function testIn()
     {
-        $this->assertEquals('u.groups IN (1, 3, 4, 7)', $this->expr->in('u.groups', array(1,3,4,7)));
+        $this->assertEquals("u.groups IN ('1', '3', '4', '7')", $this->expr->in('u.groups', array(1, 3, 4, 7)));
     }
 
     public function testInWithPlaceholder()
